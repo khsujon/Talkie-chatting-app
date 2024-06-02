311b7e90f375f3bbb232e61acf20d5b34eed7092
khsujon <kamrulhasan.km3@gmail.com>
2024-06-02 21:47:26 +0600

    x

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:talkie/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,33 +8,23 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+        debugShowCheckedModeBanner: false,
+        title: 'Talkie',
+        theme: ThemeData(
+            appBarTheme: AppBarTheme(
+          centerTitle: true,
+          elevation: 3,
+          iconTheme:
+              IconThemeData(color: const Color.fromARGB(255, 10, 81, 139)),
+          titleTextStyle: TextStyle(
+              color: Colors.greenAccent,
+              fontWeight: FontWeight.w700,
+              fontSize: 19),
+          backgroundColor: Colors.white54,
+        )),
+        home: HomeScreen());
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(CupertinoIcons.home),
+        title: const Text("Talkie"),
+        actions: [
+          //search user button
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+
+          //more feature button
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+        ],
+      ),
+
+      //floating button for add user
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 20),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add_comment_rounded),
+        ),
+      ),
+    );
+  }
+}
